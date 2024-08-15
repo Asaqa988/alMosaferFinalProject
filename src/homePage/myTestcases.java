2fe0bfd0ee4daaf3f9a60f6f470e4d6c60172d49
Asaqa988 <asaqa001@gmail.com>
2024-08-15 19:14:08 +0300

    update

--- src/homePage/myTestcases.java
+++ src/homePage/myTestcases.java
@@ -1,6 +1,9 @@
 package homePage;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,6 +18,8 @@ public class myTestcases {
 	WebDriver driver = new ChromeDriver();
 	String AlMosaferURL = "https://global.almosafer.com/en";
 	String ExpectedDefaultLanage = "en";
+	
+	Random rand = new Random(); 
 
 	@BeforeTest
 	public void mySetup() {
@@ -54,7 +59,7 @@ public class myTestcases {
 		Assert.assertEquals(ActualContactNumber, ExpectedContactNumber);
 	}
 
-	@Test
+	@Test(priority = 4)
 	public void CheckQitagLogo() {
 		boolean ExpectedResultsForTheLogo = true;
 		WebElement theFooter = driver.findElement(By.tagName("footer"));
@@ -63,9 +68,70 @@ public class myTestcases {
 				.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ekulBa.eYboXF"));
 
 		boolean ActualResultForThelogo = logo.isDisplayed();
-		
+
 		Assert.assertEquals(ActualResultForThelogo, ExpectedResultsForTheLogo);
 
 	}
+
+	@Test(priority = 5)
+	public void TestHotelTabIsNotSelected() {
+		String expectedValue = "false";
+		WebElement HotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		String ActualValue = HotelTab.getAttribute("aria-selected");
+
+		Assert.assertEquals(ActualValue, expectedValue);
+
+	}
+
+	@Test(priority = 6)
+	
+	public void CheckDepatureDate() {
+		
+		LocalDate todayDate = LocalDate.now();
+		
+	; 
+		
+	int Today  =todayDate.getDayOfMonth();
+	
+	System.out.println(Today+22);
+	
+	int Tomorrow = todayDate.plusDays(1).getDayOfMonth();
+	int ThedayAfterTomorrow = todayDate.plusDays(2).getDayOfMonth(); 
+	
+	List<WebElement> depatureAndArrivalDates = driver.findElements(By.className("LiroG")); 
+
+ 	String  ActualDepatureDate = depatureAndArrivalDates.get(0).getText(); 
+ 	String ActualReturnDate = depatureAndArrivalDates.get(1).getText(); 
+ 	
+ 	
+ 	int ActualDepatureDateAsInt = Integer.parseInt(ActualDepatureDate); 
+ 	int ActualreturnDateAsInt = Integer.parseInt(ActualReturnDate); 
+
+ 	
+
+ 	Assert.assertEquals(ActualDepatureDateAsInt, Tomorrow);
+ 	Assert.assertEquals(ActualreturnDateAsInt, ThedayAfterTomorrow);
+
+
+ 	
+
+		
+		
+	}
+	
+	
+	@Test(priority = 7)
+	
+	public void RandomlyChangeTheLanguage() {
+				String [] URLs = {"https://www.almosafer.com/en","https://www.almosafer.com/ar",}; 
+				 int RandomIndex = rand.nextInt(URLs.length) ; 
+				 
+				 driver.get(URLs[RandomIndex]); 
+			
+ 		
+	}
+	
+	
+	
 
 }
